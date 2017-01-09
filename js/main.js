027bc9720164f07a2346bd9ca1ee5bc379a1ecae
Machar1el <smt005@gmail.com>
2017-01-09 11:55:23 +0100

    Add files via upload
    Infrastructure prête pour accueillir le plateau de jeu.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ $( function() {
 
 	var newGameLinks = $(".NewGameLink");
 	var buttons = $(".contentBox button");
+	var content = $("#BG").find(".content");
   var nav = $("#nav");
+	var location;
 
 	newGameLinks.click( function() {
 
@@ -14,14 +16,46 @@ $( function() {
 
 	});
 
+	function goToKey(key) {
+
+		location = "html/" + key + ".html";
+
+			$(".content").load(location,  function(responseTxt, statusTxt, xhr) {
+	      if(statusTxt == "success")
+
+	          console.log("AJAX : " + location);
+
+						/* - Re-attachement de l'event handler puisqu'on recharge un bout de la page qui contient des boutons - */
+
+	          reloadEvents();
+
+
+	      if(statusTxt == "error")
+	          alert("Error: " + xhr.status + ": " + xhr.statusText + ", location : " + location);
+	    });
+
+	}
+
 	function startGame() {
 
-
+		goToKey("start");
 
 	}
 
 	function endGame() {
 
 	}
+
+	function reloadEvents() {
+
+    buttons = $(".content button");
+
+    buttons.click( function() {
+
+      goto = $(this).attr("go");
+      goToKey(goto);
+    } );
+
+  }
 
 } );
